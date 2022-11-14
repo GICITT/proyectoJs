db68f9f40adeafa895e1c095e128e251e0467c4b
GISELA <gisela.cittadini@gmail.com>
2022-11-14 13:09:59 -0300

    cambios

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ let eleccion;
 
 // Creo las const con los precios
 
-const iphone = 280000;
-const galaxy = 194999;
-const moto = 145999;
+const humus = 1000;
+const fertilizante = 2160;
+const fertilizanteCésped = 989;
 
 // Alert de bienvenida
 alert("Bienvenido/a! A comprar!");
@@ -20,11 +20,11 @@ function comprar() {
     eleccion = parseInt(
       prompt(`Seleccioná el producto que vas a comprar:
     
-    1- Iphone 16 = $280.000.
+    1- humus = $1.000
     
-    2- Galaxy S21 = $194.999.
+    2- fertilizante = $2.160
     
-    3- Moto G200 5g = $145.999.
+    3- fertilizante Cesped = $989
     `)
     );
   } while (eleccion !== 1 && eleccion !== 2 && eleccion !== 3);
@@ -32,13 +32,13 @@ function comprar() {
   // Creo un condicional que nos de una respuesta segun la opcion elegida
 
   if (eleccion === 1) {
-    total = iphone;
+    total = humus;
     alert("Tenes que abonar: $" + total);
   } else if (eleccion === 2) {
-    total = galaxy;
+    total = fertilizante;
     alert("Tenes que abonar: $" + total);
   } else {
-    total = moto;
+    total = fertilizanteCésped;
     alert("Tenes que abonar: $" + total);
   }
 
@@ -54,7 +54,8 @@ function comprar() {
   if (eleccion === 1) {
     comprar();
   } else {
-    alert("Chau!");
+    alert("Gracias por su compra, vuelva pronto!");
   }
 }
+
 comprar();
